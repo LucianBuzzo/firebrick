8f8d02e80e8610f9feb30f96f3f59153248ce2fe
Lucian Buzzo <lucian@gravitywell.co.uk>
2016-12-17 16:56:00 +0000

    Added a context menu with the 'Add to playlist' option.

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const url = require('url');
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-function createWindow () {
+const createWindow = function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 1024, height: 600});
 
@@ -32,7 +32,7 @@ function createWindow () {
     // when you should delete the corresponding element.
     mainWindow = null;
   });
-}
+};
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
